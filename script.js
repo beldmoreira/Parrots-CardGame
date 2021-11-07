acef9130478c607b8e8c5527b4afc22ee3dc9ca7
Isabela Moreira <isabela_cmoreira@hotmail.com>
2021-11-07 17:28:13 -0300

    Criação de prompt que determina número de cartas

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function selecionarNumero(){
- const numeroCartas = prompt("Com quantas cartas você quer jogar?")
-if (numeroCartas !== 4 14)
-
-while( )
+    let numeroCartas = prompt("Com quantas cartas você quer jogar?")
+    let numeroValido = (numeroCartas % 2 == 0) && (numeroCartas >= 4) && (numeroCartas <= 14)
+    let numeroInvalido = !numeroValido
+    while(numeroInvalido){
+        numeroCartas = prompt("Este número é inválido. Escolha números pares de 4 a 14. Com quantas cartas você quer jogar?")
+        numeroValido = (numeroCartas % 2 == 0) && (numeroCartas >= 4) && (numeroCartas <= 14)
+        numeroInvalido = !numeroValido
+       
+    }
+    return numeroCartas
 }
 
+// function virarCarta(){
 
+//}
 
 function comparador() { 
 	return Math.random() - 0.5; 
