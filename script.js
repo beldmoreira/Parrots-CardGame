ddbb41d8e8a9f9feec18c9256dc84e6bac95bcca
Isabela Moreira <isabela_cmoreira@hotmail.com>
2021-11-07 23:58:24 -0300

    Modificcaçcões na função em que se clica na carta

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const numeroMaximo = 14
+let primeiraCarta= null
+let processandoClique =false
+
 
 function iniciarJogo(){
- let numeroCartas= selecionarNumero()
- const classCartas = ["rock","unicornio","bob","fiesta","papagaioPescoço","trigemios","marinheiro"]
- let remocaoPares = (numeroMaximo-numeroCartas)/2
- for(let i=0;i< remocaoPares;i++){
-  const lista = document.querySelectorAll(`.${classCartas[i]}`) 
-   for (var item of lista) {
-    item.remove()
-  } 
- }
- let cartas = document.querySelectorAll(".container > .card")
- let cartasArray=Array.from(cartas)
- cartasArray.sort(comparador);
- let container = document.querySelector(".container")
- for (var item of cartasArray) {
-   container.appendChild(item)
- }
+    let numeroCartas= selecionarNumero()
+    const classCartas = ["rock","unicornio","bob","fiesta","papagaioPescoço","trigemios","marinheiro"]
+    let remocaoPares = (numeroMaximo-numeroCartas)/2
+    for(let i=0;i< remocaoPares;i++){
+    const lista = document.querySelectorAll(`.${classCartas[i]}`) 
+    for (var item of lista) {
+        item.remove()
+    } 
+    }
+    let cartas = document.querySelectorAll(".container > .card")
+    let cartasArray=Array.from(cartas)
+    cartasArray.sort(comparador);
+    let container = document.querySelector(".container")
+    for (var item of cartasArray) {
+    container.appendChild(item)
+    }
 } 
 
 function comparador() { 
@@ -36,13 +39,38 @@ function selecionarNumero(){
     return parseInt(numeroCartas)
 }
 
+function clicarCarta(card) {
+    if(processandoClique){
+       return 
+    }
+    if(card== primeiraCarta)
+        return
+    processandoClique= true
 
-//function virarCarta(){
-    // Inicialmente, as cartas devem estar viradas para baixo
+    if (primeiraCarta == null){
+        primeiraCarta = card
+        processandoClique = false;
 
-    // 
-//}
+    }
+    else {
+        let tipoPrimeiraCarta = document.querySelectorAll("")
+        let =Array.from()
+        card.classList.contains();
+        //Checagem se a carta é o par
+        //se não for o par, virar as duas com intervalo de 1s
+        //se for par, deixar as duas viradas
+        //setInterval(function, 1000);      
+    }
 
+    virarCarta(card)
+}
 
+function virarCarta(card){
+    //const carta = document.querySelector("");
+    card.classList.add("virado");
+
+}
+
+alert("Você ganhou em X jogadas!")
 
 iniciarJogo()
